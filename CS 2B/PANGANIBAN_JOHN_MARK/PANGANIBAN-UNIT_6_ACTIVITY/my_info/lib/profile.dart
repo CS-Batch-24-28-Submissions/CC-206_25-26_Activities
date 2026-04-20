d53134be6add62b444879a43ebdb2aab62a571c8
Dyestrm <johnmark.panganiban@wvsu.edu.ph>
2026-04-20 19:45:34 +0800

    feat: Add profile class to store data about myself

---
+++ CS 2B/PANGANIBAN_JOHN_MARK/PANGANIBAN-UNIT_6_ACTIVITY/my_info/lib/profile.dart
@@ -0,0 +1,7 @@
+abstract class Me {
+  static const String email = 'johnmark.panganiban@wvsu.edu.ph';
+  static const String education = 'West Visayas State University';
+  static const String course = 'Bachelor of Science in Computer Science';
+  static const String hobbies = 'Gaming, Listening to Music, Journaling, and Programming';
+  static const String favoriteDS = 'Lookup Table';
+}
