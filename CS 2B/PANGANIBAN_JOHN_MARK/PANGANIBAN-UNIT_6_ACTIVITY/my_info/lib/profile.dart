7581a2ff8a13fa1195ef9dded84e65a5029bf751
Dyestrm <johnmark.panganiban@wvsu.edu.ph>
2026-04-20 21:52:28 +0800

    refactor: Add biography to profile

--- CS 2B/PANGANIBAN_JOHN_MARK/PANGANIBAN-UNIT_6_ACTIVITY/my_info/lib/profile.dart
+++ CS 2B/PANGANIBAN_JOHN_MARK/PANGANIBAN-UNIT_6_ACTIVITY/my_info/lib/profile.dart
@@ -4,4 +4,6 @@ abstract class Me {
   static const String course = 'Bachelor of Science in Computer Science';
   static const String hobbies = 'Gaming, Listening to Music, Journaling, and Programming';
   static const String favoriteDS = 'Lookup Table';
+
+  static const String biography = "You can call me JM. I'm a Computer Science student at West Visayas State University who cares about writing clean, readable code, with a special interest in behavioral patterns, data structures, and object oriented programming. Outside of coding, I enjoy video games, music, and taking quiet moments to reflect on my day. Nice to meet you!";
 }
