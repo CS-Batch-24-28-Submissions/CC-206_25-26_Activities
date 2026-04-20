89fd54938062885c42433c471ed90b4a6bbf0065
Dyestrm <johnmark.panganiban@wvsu.edu.ph>
2026-04-20 22:38:13 +0800

    chore: Rename file and add fullName field

--- CS 2B/PANGANIBAN_JOHN_MARK/PANGANIBAN-UNIT_6_ACTIVITY/my_info/lib/about_me.dart
+++ CS 2B/PANGANIBAN_JOHN_MARK/PANGANIBAN-UNIT_6_ACTIVITY/my_info/lib/about_me.dart
@@ -1,4 +1,5 @@
 abstract class Me {
+  static const String fullName = 'John Mark Panganiban';
   static const String email = 'johnmark.panganiban@wvsu.edu.ph';
   static const String education = 'West Visayas State University';
   static const String course = 'Bachelor of Science in Computer Science';
