a64e89f9a11231425bbd40b04971aec0eeef8a5b
karukentu <alconadakarlkenth07@gmail.com>
2026-05-24 20:16:17 +0800

    Add ALCONADA_ACTIVITY_6 Flutter profile app

---
+++ CS 2A/ALCONADA_KARL_KENTH/ALCONADA_ACTIVITY_6/simple_profile/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\alcon_9l9zdrf\CC-206_25-26_Activities\CS 2A\ALCONADA_KARL_KENTH\ALCONADA_ACTIVITY_6\simple_profile"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
